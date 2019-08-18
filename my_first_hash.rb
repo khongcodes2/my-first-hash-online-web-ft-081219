5e9f4ee69ea76f8e3c9224c6bf0924119e2fb544
K Hong <albtrossfacefall@gmail.com>
2019-08-18 14:42:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["oil paintings"]
+  shipping_manifest["oil paintings"]
 
 end
 
@@ -36,7 +36,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest[]
 
   # add 4 gun powder to the shipping_manifest hash below
 
